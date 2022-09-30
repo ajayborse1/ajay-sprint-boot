19a24b9b5d4e3e68296651b6794cc57dd62bdc3c
Ajay <ajayborse90@gmail.com>
2022-09-30 18:02:19 +0530

    New Repo

--- src/main/java/com/jpa/test/service/StudentService.java
+++
@@ -1,8 +0,0 @@
-package com.jpa.test.service;
-import java.util.ArrayList;
-import com.jpa.test.model.Student;
-
-public interface StudentService {
-    public ArrayList<Student> getStudent(Integer id, String name);
-    public Student saveStudent(Student id);
-}
---
+++ src/main/java/com/wipro/bankofamerica/estore/service/EmployeeService.java
@@ -0,0 +1,20 @@
+package com.wipro.bankofamerica.estore.service;
+
+import java.util.List;
+
+import com.wipro.bankofamerica.estore.model.Employee;
+
+/**
+ *
+ * @author Jeevan
+ */
+public interface EmployeeService {
+
+	public Employee saveEmployee(Employee employee);
+	
+	public Employee getEmployee(Integer id);
+
+	public List<Employee> getEmployeeCity(String city);
+
+
+}
